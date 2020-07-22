b1eb8a4a6a5a3dff127c6c62844073676dc24141
emanuelecuzari <emanuele-cuzari@outlook.it>
2020-07-22 12:25:18 +0200

    final commit

--- include/queue.h
+++ include/queue.h
@@ -1,11 +1,17 @@
+/**
+ * interfaccia per la definizione di una coda unbounded
+*/
+
 #if !defined(QUEUE_H)
 #define QUEUE_H
 
+/* struttura per definizione dei nodi */
 typedef struct Node{    
     void* info;
     struct Node* next;
 }Node_t;
 
+/* struttura per definizione della coda */
 typedef struct LQueue{     
     Node_t* head;
     Node_t* tail;
@@ -13,11 +19,30 @@ typedef struct LQueue{
 }LQueue_t;
 
 LQueue_t* init_LQueue();
+/**
+ * funzione per inizializzaione coda
+ * @return q success, NULL failure
+*/
 
 void deleteLQueue(LQueue_t* q);
+/**
+ * funzione per cancellazione coda
+ * @param q è coda da cancellare
+*/
 
 int insertLQueue(LQueue_t* q, void* arg);
+/**
+ * funzione per inserimento nuovo nodo
+ * @param q coda in cui inserire
+ * @param arg dato da inserire
+ * @return 0 success, -1 failure
+*/
 
 void* popLQueue(LQueue_t* q);
+/**
+ * funzione per rimozione nodo da coda
+ * @param q coda da cui estrarre dato
+ * @return dato estratto success, NULL failure
+*/
 
 #endif /* QUEUE_H */
